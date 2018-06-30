069bc61f3aaf4c8e2c85faf282385227a8e8dccf
shamnex <shamnex@icloud.com>
2018-06-30 07:20:10 +0100

    added responsiveness

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const CACHE_STATIC = "konvatam-skeleton-v37";
+const CACHE_DYNAMIC = "konvatam-dynamic";
 const APP_SHELL_URLS = [
     '/',
     '/index.html',
@@ -5,13 +7,26 @@ const APP_SHELL_URLS = [
     '/img/bg_pattern_white.svg',
     '/img/bg_pattern.svg',
     '/scripts/bundle.js',
-
 ]
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open("konvatam-skeleton-1")
+        caches.open(CACHE_STATIC)
             .then((cache) => cache.addAll(APP_SHELL_URLS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then(keysArray => {
+                return Promise.all(keysArray.map((key) => {
+                    if (key !== CACHE_STATIC && key !== CACHE_DYNAMIC ) {
+                        console.log("Removing old cache");
+                        return caches.delete(key);
+                    }
+                }))
+            })
     );
 });
 
@@ -24,13 +39,13 @@ self.addEventListener('fetch', (event) => {
                 } else {
                     return fetch(event.request)
                         .then((res) => {
-                            return caches.open("konvatam-dynamic")
+                            return caches.open(CACHE_DYNAMIC)
                                 .then((cache) => {
                                     cache.put(event.request.url, res.clone());
                                     return res;
                                 })
-                        }).catch((error)=> {
-                            
+                        }).catch((error) => {
+
                         })
                 }
             })
